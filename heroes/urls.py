a600e751de14fa83f649a5e02ecc05617a685a6e
DEV-T-2025 <arthuramerico33@gmail.com>
2025-09-03 16:25:04 -0300

    criado modo forms e adicionar herois

--- heroes/urls.py
+++ heroes/urls.py
@@ -1,8 +1,16 @@
 
 from django.urls import path
-from .views import HeroListView , lista_herois
+from .views import HeroListView , lista_herois , contato_view , criar_heroi
+
 
 urlpatterns = [
     path('lista/', lista_herois, name='lista_herois'),
     path('cbv-lista/', HeroListView.as_view(), name='cbv_lista_herois'),
+    path('contato/', contato_view, name='contato'),
+    path('novo/', criar_heroi, name='criar_heroi'),
 ]
+
+
+
+
+
--- heroes/views.py
+++ heroes/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .models import Hero
 from django.views.generic import ListView
+from .forms import ContatoForm , HeroForm
 
+def contato_view(request):
+    form = ContatoForm()  # formulário vazio
+
+    if request.method == "POST":
+        form = ContatoForm(request.POST)
+        if form.is_valid():
+            # Aqui você poderia enviar um e-mail ou salvar no banco
+            print(form.cleaned_data)
+            return render(request, "heroes/contato_sucesso.html")
+
+    return render(request, "heroes/contato.html", {"form": form})
 
 def lista_herois(request):
     herois = Hero.objects.all()  # busca todos os heróis do banco
@@ -11,3 +23,15 @@ class HeroListView(ListView):
     model = Hero
     template_name = "heroes/lista_herois.html"
     context_object_name = "herois"
+
+
+def criar_heroi(request):
+    if request.method == "POST":
+        form = HeroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_herois')
+    else:
+        form = HeroForm()
+
+    return render(request, "heroes/form_heroi.html", {"form": form})
